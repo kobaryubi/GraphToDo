23dc9465eda01a3fc735f28e8d7baccb83341e83
Masahiko Kobayashi <kobaryubi6@gmail.com>
2023-11-12 18:11:36 +0900

    Initial Commit

---
+++ GraphToDo/GraphToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphToDoApp.swift
+//  GraphToDo
+//
+//  Created by Masahiko Kobayashi on 2023/11/12.
+//
+
+import SwiftUI
+
+@main
+struct GraphToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
