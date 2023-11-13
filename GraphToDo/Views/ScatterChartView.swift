71db981129de6a632b0443605fe7573358d5aab2
Masahiko Kobayashi <kobaryubi6@gmail.com>
2023-11-13 21:41:56 +0900

    feat: add scatter chart view

---
+++ GraphToDo/Views/ScatterChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ScatterChartView.swift
+//  GraphToDo
+//
+//  Created by Masahiko Kobayashi on 2023/11/12.
+//
+
+import SwiftUI
+
+struct ScatterChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScatterChartView()
+}
