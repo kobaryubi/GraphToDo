99f2302dd45d6b9ed0408a5dbadf490c9ba3942d
Masahiko Kobayashi <kobaryubi6@gmail.com>
2023-11-16 15:08:15 +0900

    feat: add create view

---
+++ GraphToDo/Views/ToDoCreateView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoCreateView.swift
+//  GraphToDo
+//
+//  Created by Masahiko Kobayashi on 2023/11/16.
+//
+
+import SwiftUI
+
+struct ToDoCreateView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ToDoCreateView()
+}
